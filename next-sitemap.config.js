96153b24537af8b861137b3f2d10eca99bfc8367
Anastassia Vomm <anastassia.vomm@gmail.com>
2024-04-30 21:03:45 +0300

    seo optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://viimsiuisuplats.ee",
+  generateRobotsTxt: true,
+  alternateRefs: [
+    "https://viimsiuisuplats.ee/en",
+    "https://viimsiuisuplats.ee/et",
+    "https://viimsiuisuplats.ee/ru",
+  ],
+};
